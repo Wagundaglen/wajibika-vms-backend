0e4cf4788a976a2166eca3a58e2d01e9e7bfc35f
Wagundaglen <glenwagunda@gmail.com>
2025-08-28 10:47:22 +0300

    Updated codes for vms project

--- feedback/forms.py
+++ feedback/forms.py
@@ -12,26 +12,27 @@ class FeedbackForm(forms.ModelForm):
             'message': forms.Textarea(attrs={'rows': 5}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
         }
-    
+
     def __init__(self, *args, **kwargs):
+        # safely pop the request so it doesn’t break BaseModelForm
+        self.request = kwargs.pop("request", None)
         super().__init__(*args, **kwargs)
+
         self.fields['category'].queryset = FeedbackCategory.objects.all()
         self.fields['category'].empty_label = "Select a category"
-        
-        # Make anonymous fields required if anonymous is checked
+
         self.fields['anonymous_name'].required = False
         self.fields['anonymous_email'].required = False
-    
+
     def clean(self):
         cleaned_data = super().clean()
         is_anonymous = cleaned_data.get('is_anonymous')
         anonymous_name = cleaned_data.get('anonymous_name')
-        
+
         if is_anonymous and not anonymous_name:
             raise forms.ValidationError(
                 "Please provide a name for anonymous feedback."
             )
-        
         return cleaned_data
 
 class FeedbackResponseForm(forms.ModelForm):
